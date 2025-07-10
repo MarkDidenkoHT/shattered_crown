5b36aca395cb191b767f9400c1e2f5a55dcb0efb
Mark <didenko.m@hi-tech.md>
2025-07-10 09:18:55 +0300

    Update character_creation.js

--- public/character_creation.js
+++ public/character_creation.js
@@ -1,1 +1,8 @@
-
+export async function loadModule(main, { currentSession, supabaseConfig, getCurrentProfile, getCurrentSession, authenticatedFetch }) {
+  main.innerHTML = `
+    <div class="character-creation-container">
+      <h2>Character Creation</h2>
+      <p>This is a placeholder for the character creation module.</p>
+    </div>
+  `;
+}
