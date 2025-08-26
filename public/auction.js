567f336dd4ff8291361891485bbd8fe0e6d581d7
Mark <didenko.m@hi-tech.md>
2025-08-26 17:29:19 +0300

    Update auction.js

--- public/auction.js
+++ public/auction.js
@@ -12,7 +12,6 @@ export async function loadModule(main, { apiCall, getCurrentProfile }) {
     _main = main;
     _apiCall = apiCall;
     _getCurrentProfile = getCurrentProfile;
-    setBankHeaderBackground();
 
     _profile = _getCurrentProfile();
     if (!_profile) {
@@ -135,6 +134,7 @@ export async function loadModule(main, { apiCall, getCurrentProfile }) {
     createParticles();
     setupAuctionInteractions();
     await loadCurrentView();
+    setBankHeaderBackground();
 }
 
 async function loadCurrentView() {
